6f329f0c643337c65b782fc85357c174d6fb073c
Matt <matt@primestudiosllc.com>
2012-02-02 07:50:19 -0800

    updated facebook production app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == "production"
-    #provider :facebook, '222312241193666', '24b31ac6bd0fb524d012536242eacd45'
+    provider :facebook, '103826493075115', '0441da7154ffe57f61124e3d27d4d8a9'
   else
     provider :facebook, '103230106469740', '07d0543dce1859249f74d08b7649dd30'
   end
