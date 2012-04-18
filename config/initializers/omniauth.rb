cf82160a8024ae2eeac4c6ef6c18044d183c6903
Matt <matt@primestudiosllc.com>
2012-04-18 02:34:49 -0700

    no config

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env == "production"
-    provider :facebook, '103826493075115', '0441da7154ffe57f61124e3d27d4d8a9'
-  else
-    provider :facebook, '103230106469740', '07d0543dce1859249f74d08b7649dd30'
-  end
-end
--- config/initializers/twilio.rb
+++
@@ -1,3 +0,0 @@
-Twilio::Config.setup \
-  :account_sid  => 'AC66dd04812ce948b3ae6d2e241f36fb84',
-  :auth_token   => 'd21e6382ed9bb6bc6f086d20e8b39275'
